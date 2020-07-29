e4e2383c1de73a047ac1a21bff2b34cb1b59d8a8
Javier Belmonte <belmontj@purdue.edu>
2020-07-28 20:13:39 -0400

    Included PPG as an Arduino library

--- Firmware/Fyre/Fyre.ino
+++ Firmware/Fyre/Fyre.ino
@@ -1,9 +1,12 @@
+/* Fyre Smartwatch
+ */
+ 
+#include <KickJrPPG.h>
+
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
